87668034442952404a5c372a79217bdbe6356bbf
Artyom <zik21b@gmail.com>
2024-12-23 23:08:30 +0300

    Изменил файл scripts.py
    - Уменьшил кол-во обращений к бд

--- scripts.py
+++ scripts.py
@@ -1,13 +1,11 @@
 from datacenter.models import Subject, Lesson, Chastisement, Mark, Commendation, Schoolkid
-from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+from django.db.models import Q
 import random
 
 def fix_marks(student_name):
     student = check_name(student_name)
-    bad_ratings = Mark.objects.filter(schoolkid=student, points__lt=4)
-    for bad_rating in bad_ratings:
-        bad_rating.points = random.randint(4, 5)
-        bad_rating.save()
+    Mark.objects.filter(Q(schoolkid=student) & Q(points__lt=4)).update(points=random.randint(4, 5))
 
 def remove_chastisements(student_name):
     student = check_name(student_name)
@@ -16,27 +14,24 @@ def remove_chastisements(student_name):
 
 def create_commendation(student_name, subject):
     student = check_name(student_name)
-    school_subject = Subject.objects.filter(title=check_subject(subject), year_of_study=str(student.year_of_study))
+    school_subject = check_subject(subject, student.year_of_study)
     lesson = random.choice(Lesson.objects.filter(subject=school_subject))
     with open('comments.txt', 'r', encoding='utf-8') as file:
         comments = random.choice(file.readlines())
     Commendation.objects.create(schoolkid=student, subject=school_subject, text=comments, created=lesson.date, teacher=lesson.teacher)
 
 def check_name(student_name):
-    student = Schoolkid.objects.filter(full_name__contains=student_name)
     try:
-        student = student.get(id=2)
-        student = student.get(id=0)
-        student = student[0]
-        return student[0]
-    except ObjectDoesNotExist as error:
-        return error + '\nВведене некорректные данные'
+        student = Schoolkid.objects.get(full_name__contains=student_name)
+        return student
+    except MultipleObjectsReturned:
+        raise ValueError('Найдено несколько учеников с таким именем. Пожалуйста, уточните запрос.')
+    except ObjectDoesNotExist:
+        raise ValueError('Введены некорректные данные. Ученик не найден.')
 
-def check_subject(item_name):
-    subject = Subject.objects.filter(title=item_name)
+def check_subject(item_name, year_of_study):
     try:
-        subject = subject.get(id=0)
-        subject = subject[0]
-        return subject.title
-    except ObjectDoesNotExist as error:
-        return error + '\nВведене некорректные данные'
+        subject = Subject.objects.get(title=item_name, year_of_study=year_of_study)
+        return subject
+    except ObjectDoesNotExist:
+        raise ValueError('Введены некорректные данные. Предмет не найден.')
