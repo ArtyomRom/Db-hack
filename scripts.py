7379277c5a2beabe620c30d01da7713975bf7bcc
Artyom <zik21b@gmail.com>
2024-12-25 18:43:13 +0300

    Убрал лишние импорты, изменил исключения для каждой конкретной модели

--- scripts.py
+++ scripts.py
@@ -1,11 +1,14 @@
-from datacenter.models import Subject, Lesson, Chastisement, Mark, Commendation, Schoolkid
-from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
-from django.db.models import Q
 import random
+from django.core.exceptions import MultipleObjectsReturned
+from django.db.models import DoesNotExist
+
+from datacenter.models import Subject, Lesson, Chastisement, Mark, Commendation, Schoolkid
+
+
 
 def fix_marks(student_name):
     student = check_name(student_name)
-    Mark.objects.filter(Q(schoolkid=student) & Q(points__lt=4)).update(points=random.randint(4, 5))
+    Mark.objects.filter(schoolkid=student, points__lt=4).update(points=random.randint(4, 5))
 
 def remove_chastisements(student_name):
     student = check_name(student_name)
@@ -24,14 +27,14 @@ def check_name(student_name):
     try:
         student = Schoolkid.objects.get(full_name__contains=student_name)
         return student
-    except MultipleObjectsReturned:
+    except Schoolkid.MultipleObjectsReturned:
         raise ValueError('Найдено несколько учеников с таким именем. Пожалуйста, уточните запрос.')
-    except ObjectDoesNotExist:
+    except Schoolkid.DoesNotExist:
         raise ValueError('Введены некорректные данные. Ученик не найден.')
 
 def check_subject(item_name, year_of_study):
     try:
         subject = Subject.objects.get(title=item_name, year_of_study=year_of_study)
-        return subject
-    except ObjectDoesNotExist:
+        return subjec
+    except Subject.DoesNotExist:  # Здесь тоже используем специфичное исключение
         raise ValueError('Введены некорректные данные. Предмет не найден.')
